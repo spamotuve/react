9fd5c3987258bc3e9e050f713979cfb45e69e142
Birznieks <bruno.birznieks@accenture.com>
2017-09-04 17:52:23 +0300

    first commit

---
+++ src/app/components/Home.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <p>In a new Component!</p>
+      </div>
+    );
+  }
+}
